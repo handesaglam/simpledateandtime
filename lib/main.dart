0bc4b2afdc12dab68ae65075634fa04c546fb51c
GRUNDIG <handesaglam70@gmail.com>
2020-11-06 13:11:52 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'date_time_picker_widget2.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+
+      ),
+
+      body: Center(
+        child: DateTimePickerWidget2(),
+
+      ),
+
+
+
+    );
+  }
+}
